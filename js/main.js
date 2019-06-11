bc8a654df29c3bb24df8bb3aa175ad9d45fc7b3e
alexgamez <alex_gamez93@hotmail.com>
2019-06-11 15:59:46 -0500

    Slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function(){
+  $(function(){
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 1200,
+      responsive: true
+    });
+  });
+});
